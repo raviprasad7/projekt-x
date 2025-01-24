93aac7e681966e5382facdb933abe3b7d3b1e7f9
Ravi Prasad <ravi070994@gmail.com>
2025-01-24 14:22:48 +0530

    Add theme provider

--- src/app/AppProvider.tsx
+++ src/app/AppProvider.tsx
@@ -1,13 +1,16 @@
+'use client';
+
 import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Toaster } from 'projekt-x/components/ui/toaster';
+import { ThemeProvider } from 'projekt-x/components/ui/ThemeProvider';
 const queryClient = new QueryClient();
 
 export const AppProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <QueryClientProvider client={queryClient}>
-      {children}
+      <ThemeProvider>{children}</ThemeProvider>
       <Toaster />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
