2526c2b99a157cdfb7d4781d26b1c31995b2ceb3
Ravi Prasad <ravi070994@gmail.com>
2025-01-24 11:23:04 +0530

    Add basic project setup; Add formating and linting rules;

---
+++ src/app/AppProvider.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Toaster } from 'projekt-x/components/ui/toaster';
+const queryClient = new QueryClient();
+
+export const AppProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <Toaster />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
---
+++ src/components/ui/ThemeProvider.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import React from 'react';
+import { ThemeProvider as NextThemesProvider, ThemeProviderProps } from 'next-themes';
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return (
+    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange {...props}>
+      {children}
+    </NextThemesProvider>
+  );
+}
